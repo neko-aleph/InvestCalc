075af05fe760a7f2e2a9b36efbdba8c52fd2465b
Pavel <barikovp@icloud.com>
2024-05-15 22:19:00 +0600

    Add more unit tests

--- tests.py
+++ tests.py
@@ -8,3 +8,12 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.calculate(100000, 12, 12, 12)[11]['amount'], '112682.50')
         self.assertEqual(calculator.calculate(100000, 12, 12, 4)[1]['income'], '6090.00')
         self.assertEqual(calculator.calculate(100000, 12, 12, 2)[0]['number'], '6')
+
+
+class TestApp(unittest.TestCase):
+    def test_index(self):
+        ...
+
+
+    def test_calculate(self):
+        ...
